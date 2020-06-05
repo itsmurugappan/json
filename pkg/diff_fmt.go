4615fb37dc564ec8d127c556469e5532a8d51b35
itsmurugappan <itsmurugappan@gmail.com>
2020-06-05 15:08:11 -0700

    first commit

---
+++ pkg/diff_fmt.go
@@ -0,0 +1,33 @@
+package pkg
+
+import (
+	"gotest.tools/assert"
+	"testing"
+)
+
+func TestJSONDiff(t *testing.T) {
+	for _, tc := range []struct {
+		inputA   string
+		inputB   string
+		expected []string
+	}{{
+		`{"a": "1", "b": [{"a": "2"},{"a": "3"}], "c": {"a": "1"}}`,
+		`{"c": {"a": "1", "b": "1"}, "a": "2", "b": [{"a": "2"},{"a": "3"}]}`,
+		[]string{"a", "c"},
+	}, {
+		`{"a": "1", "b": [{"a": "2"},{"a": "3"}], "c": {"a": "1"}}`,
+		`{"c": {"a": "1", "b": "1"}, "a": "2", "b": [{"a": "4"},{"a": "5"}]}`,
+		[]string{"a", "b", "c"},
+	}, {
+		`{"a": "1", "b": [{"a": "2"},{"a": "3"}], "c": {"a": "1"}}`,
+		`{"a": "1", "b": [{"a": "2"},{"a": "3"}], "c": {"a": "1"}}`,
+		nil,
+	}, {
+		`{"a": "1", "b": ["a": "3"},{"a": "3"}], "c": {"a": "1"}}`,
+		`{"a": "1", "b": [{"a": "2"},{"a": "3"}], "c": {"a": "1"}}`,
+		nil,
+	}} {
+		act := Diff(tc.inputA, tc.inputB)
+		assert.DeepEqual(t, &tc.expected, &act)
+	}
+}
