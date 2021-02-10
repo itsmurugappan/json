bf227ece448c786447d7b7c7ed73e990a495dcfa
itsmurugappan <itsmurugappan@gmail.com>
2021-02-09 22:32:50 -0800

    change map func

--- map.go
+++ map.go
@@ -26,3 +26,24 @@ func SpecFromMap(data map[string]string, spec interface{}) error {
 
 	return nil
 }
+
+// SpecFromMap parses the given map into the spec
+func SpecFromMapInterface(data map[string]interface{}, spec interface{}) error {
+	if data == nil {
+		return fmt.Errorf("data is nil")
+	}
+	decodeCfg := &mapstructure.DecoderConfig{
+		Metadata: nil,
+		Result:   spec,
+		TagName:  "json",
+	}
+	decoder, err := mapstructure.NewDecoder(decodeCfg)
+	if err != nil {
+		return err
+	}
+	if err := decoder.Decode(data); err != nil {
+		return err
+	}
+
+	return nil
+}
